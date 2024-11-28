65dc3230391bc7f69b92d8ba0fb9d68fe7e7c028
Oskaripellikka <oskari.pellikka@tuni.fi>
2024-11-28 11:48:13 +0200

    ensimmäinen commit

---
+++ add.test.js
@@ -0,0 +1,17 @@
+import add from './add.js'
+
+describe('Testing add function', () => {
+    test('Adding positive numbers',() => {
+        expect(add(1,5)).toBe(6);
+    });
+
+    test('Adding negative numbers',() => {
+        expect(add(-2,-4)).toBe(-6);
+    });
+    test('Adding negative and positive numbers',() => {
+        expect(add(9,-3)).toBe(6);
+    });
+    test('Checking that inputs are numbers, and throwing error if not',() => {
+        expect(() => add('b', 'o')).toThrow('Inputs must be numbers');
+    });
+})
