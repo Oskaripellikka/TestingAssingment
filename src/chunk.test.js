c6741d1cf7f0e3d71cd0a4383c26c7bd13421236
Oskaripellikka <oskari.pellikka@tuni.fi>
2024-12-09 15:57:50 +0200

    Added chunk.test.js to test chunk.js. Test results fail because of chunk.js not working.

---
+++ src/chunk.test.js
@@ -0,0 +1,21 @@
+import chunk from './chunk.js'
+
+test('Testing that chunk splits arrays to right sizes',() => {
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    expect(chunk(arr, 3)).toEqual([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+});
+
+test('Testing that chunk returns empty if array is empty',() => {
+    const arr = []
+    expect(chunk(arr)).toEqual([]);
+});
+
+test('Test that chunk size greater than array length works', () => {
+    const arr = [1, 2];
+    expect(chunk(arr, 5)).toEqual([[1, 2]]);
+});
+
+test('Testing that chunk splits arrays to right sizes with chars',() => {
+    const strarr = ['a', 'b', 'c', 'd'];
+    expect(chunk(strarr, 2)).toEqual([['a', 'b'], ['c', 'd']]);
+});
